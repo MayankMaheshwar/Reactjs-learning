860ead10dfdc0da8f33c97a9033cbc4f2389ff84
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2025-04-25 10:54:07 +0530

    initial react setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import Header from "./Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Header from "./components/Header.jsx"
+import Card from "./components/Card.jsx"
 
 
 function App() {
 
   return (
+    <>
     <Header/>
+      <h1>Welcome to my React App</h1>
+      <p>This is a simple React application.</p>
+      <p>It is built using Vite and React.</p>
+      <p>It is a simple application to demonstrate the use of React.</p>
+      <p>It is a simple application to demonstrate the use of React.</p>
+      <Card/>
+    <Footer/>
+    </>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card  = () => {
+  return (
+    <div className='card'>
+        <h1>
+            Card Component
+        </h1>
+        <p>
+            This is a simple card component.
+        </p>
+    </div>
+
+  )
+}
+
+export default Card
