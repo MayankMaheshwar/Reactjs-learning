4d98f972f4089b228dcb2cbeb05f4fe3d05a6d79
Mayank Maheshwari <mayank.maheshwari625@gmail.com>
2025-04-25 11:08:48 +0530

    built components with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
 import Card from "./components/Card.jsx"
-
+import Student from "./components/Student.jsx"
 
 function App() {
 
@@ -13,6 +13,8 @@ function App() {
       <p>It is built using Vite and React.</p>
       <p>It is a simple application to demonstrate the use of React.</p>
       <p>It is a simple application to demonstrate the use of React.</p>
+      <Student name='Mayank' age={23}/>
+      <Student name='Mahesh' age={24}/>
       <Card/>
     <Footer/>
     </>
---
+++ src/components/Student.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Student = (props) => {
+  return (
+    <div>Name: {props.name}, age: {props.age}</div>
+  )
+}
+
+ 
+export default Student
